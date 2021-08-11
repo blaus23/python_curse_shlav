4b7282c84258912d2c54f3187c1aab3eb9007214
Yuval315 <73549896+Yuval315@users.noreply.github.com>
2021-08-11 15:19:26 +0300

    Update 4fun.py

--- 4Fun/4fun.py
+++ 4Fun/4fun.py
@@ -25,6 +25,7 @@ from pymongo import MongoClient
 from datetime import datetime
 from werkzeug.utils import secure_filename
 from pathlib import Path
+from Pac.pac import parents_and_children
 
 
 app = Flask(__name__)
@@ -111,29 +112,6 @@ def reply(post_id):
 def img(img_path):
     return send_from_directory(str(upload_folder.absolute()), img_path)
 
-
-def parents_and_children(all_posts):
-    """
-    Given a list of raw posts from the DB, return a dictionary where the keys are thread posts and
-    their values are all of the thread replies.
-
-    >>> parents_and_children([
-    ...     {'parent': None, "id": 1}, # thread, because parent=null
-    ...     {'parent': 1, "id": 2},    # reply, because parent references another post
-    ...     {'parent': None, "id": 3},
-    ...     {'parent': 1, "id": 4},
-    ... ])
-    {1: [2, 4], 3: []}
-    """
-    result = {}
-    for post in all_posts:
-        if post["parent"] is None:
-            result[post["id"]] = []
-        else:
-            if not result.get(post["parent"], None):
-                result[post["parent"]] = []
-            result[post["parent"]].append(post["id"])
-    return result
 
 
 def parse_content(lines):
