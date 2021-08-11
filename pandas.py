6bc150e2dc6cbcb2f516dc7c9cc3c0a13d3e6578
Yuval315 <73549896+Yuval315@users.noreply.github.com>
2021-08-11 11:47:16 +0300

    Update pandas.py

--- pandas.py
+++ pandas.py
@@ -1,45 +1,45 @@
 import pandas as pd
 
-#1 - noy
+#1 - Noy
 read_file = pd.read_excel ("Automobile_data.xlsx", sheet_name='in')
 read_file.to_csv ("Automobile_data.csv", index = None, header=True)
 df = pd.read_csv("Automobile_data.csv")
 print(df.head(5))
 print(df.tail(5))
 
-#2 - adi
+#2 - Adi
 
 
-#3 - noy
+#3 - Noy
 high_val = df.sort_values(by="price", ascending=False)
 high_val = high_val[["company", "price"]]
 print("Highest price:\n", high_val.head(1))
 
-#4 - noam
+#4 - Noam
 df = pd.read_csv(r"C:\Users\User\PycharmProjects\adi\Automobile_data.csv")
 toy = df["company"].isin(["toyota"])
 print(df[toy])
 
-#5 = maya
+#5 = Maya
 df = pd.read_csv(r'C:/Users/User/Automobile_data.csv')
 count = df['company'].value_counts()
 print(count)
 
-#6 - ofir
+#6 - Ofir
 df = pd.read_csv('Automobile_data.csv')
 df2 = df.groupby('company').max('price')
 df2['company'] = df2.index
 print(df2[['company', 'price']])
 
-#7 - amit
+#7 - Amit
 df = pd.read_csv('Automobile_data.csv')
 print(df.groupby(['company'])['average-mileage'].agg(lambda x: x.unique().sum()/x.nunique()))
 
-#8 - ofir
+#8 - Ofir
 df = pd.read_csv('Automobile_data.csv')
 print(df.sort_values(by='price', ascending=False))
 
-#9 - yuval
+#9 - Yuval
 GermanCars = {'Company': ['Ford', 'Mercedes', 'BMV', 'Audi'], 'Price': [23845, 171995, 135925 , 71400]}
 japaneseCars = {'Company': ['Toyota', 'Honda', 'Nissan', 'Mitsubishi '], 'Price': [29995, 23600, 61500 , 58900]}
 GermanCars = pd.DataFrame.from_dict(GermanCars)
@@ -47,7 +47,7 @@ japaneseCars = pd.DataFrame.from_dict(japaneseCars)
 df_row = pd.concat([GermanCars, japaneseCars])
 print(df_row)
 
-#10 - ofir
+#10 - Ofir
 Car_Price = {'Company': ['Toyota', 'Honda', 'BMW', 'Audi'],
              'Price': [23845, 17995, 135925, 71400]}
 Car_Horsepower = {'Company': ['Toyota', 'Honda', 'BMW', 'Audi'],
