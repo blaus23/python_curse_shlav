afadd7d927d2001cd27651182b52284707d57e14
Yuval315 <73549896+Yuval315@users.noreply.github.com>
2021-08-11 16:32:57 +0300

    Update 4fun.py

--- 4Fun/4fun.py
+++ 4Fun/4fun.py
@@ -74,12 +74,6 @@ def id_for_new_post():
 @app.errorhandler(404)
 def page_not_found(e):
     return render_template('404.html'), 404
-
-# Custom 500 page
-@app.errorhandler(500)
-def page_not_found(e):
-    return render_template('500.html'), 500
-
    
 @app.route("/")
 def index():
