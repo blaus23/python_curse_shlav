66b41ce809a9464cd49214b3d4d3261158880d01
Yuval315 <73549896+Yuval315@users.noreply.github.com>
2021-08-12 14:02:33 +0300

    Create create_ssl.py

---
+++ 4Fun_New/create_ssl.py
@@ -0,0 +1,3 @@
+#!/bin/usr/python3
+imprt os
+os.system("mkdir ssl && openssl req -x509 -newkey rsa:4096 -nodes -out ssl/cert.pem -keyout ssl/key.pem -days 365 -subj "/C=US/ST=New York/L=New York/O=General Org/OU=Ou/CN=example.com"")
